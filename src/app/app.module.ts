9f90010363acb628f5609f88c238e5b287470638
Alexandru Fatu <alexandru.fatu@orange.com>
2018-01-30 11:55:12 +0200

    added logo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MultiauthAmonComponent } from './multiauth-amon/multiauth-amon.component';
+import { LogoComponent } from './logo/logo.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    MultiauthAmonComponent
+    MultiauthAmonComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule
